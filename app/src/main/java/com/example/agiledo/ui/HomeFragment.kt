0f0180d94bf0efb7dd82faef80f60a8118ea344e
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-09-10 01:27:14 +0300

    create add dialog

--- app/src/main/java/com/example/agiledo/ui/HomeFragment.kt
+++ app/src/main/java/com/example/agiledo/ui/HomeFragment.kt
@@ -1,18 +1,23 @@
 package com.example.agiledo.ui
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ItemTouchHelper
+import com.example.agiledo.R
 import com.example.agiledo.data.DataManager
 import com.example.agiledo.data.domain.Task
+import com.example.agiledo.databinding.FragmentAddDialogBinding
 import com.example.agiledo.databinding.FragmentHomeBinding
 import com.example.agiledo.ui.adapters.TaskAdapter
+import com.google.android.material.textfield.TextInputEditText
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() , TaskInteractionListener {
     //region initilize variables
     override val LOG_TAG: String = "HOME_FRAGMENT"
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding = FragmentHomeBinding::inflate
     lateinit var adapter : TaskAdapter
+
     //endregion
 
     //region setup
@@ -32,37 +37,43 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() , TaskInteractionListen
           val cardView = binding?.taskRecyclerView
 
 
+        }
 
-      }
 
     //endregion
     //region  addCallbacks
     override fun addCallbacks() {
+
         binding?.addTask?.setOnClickListener {
-            addNewTask()
+           openAddDialog()
+        }
+    }
+
+    private fun openAddDialog() {
+        val addDialogView = LayoutInflater.from(context).inflate(R.layout.fragment_add_dialog, null) //Inflate the dialog with add view
+        var dialogBinding = FragmentAddDialogBinding.bind(addDialogView)
+        val mBuilder = AlertDialog.Builder(context)
+            .setView(addDialogView)
+            .setTitle("Add New Task")
+        val mAlertDialog = mBuilder.show()  //show dialog
+
+
+        //confirm add button of add layout ,
+        dialogBinding.confirmAdd.setOnClickListener{
+            mAlertDialog.dismiss()
+            val newTask = Task(
+                taskName=  dialogBinding.taskNameInputEdittext.text.toString(),
+                taskDescription = dialogBinding.taskDescriptionInputEdittext.text.toString(),
+                taskStartDate = dialogBinding.startDateInputEdittext.text.toString(),
+                taskDueDate = dialogBinding.dueDateInputEdittext.text.toString(),
+                taskAssignedTo = dialogBinding.assignToInputEdittext.text.toString()
+            )
+            DataManager.addTask(newTask)
+            adapter.setData(DataManager.tasks)
         }
     }
     //endregion
 
-    //region addNewTask
-    /**
-     * this function add new task of type object of class Task to the list of task
-     * @param nothing
-     * @return nothing
-     * @author Anwar
-     */
-    private fun addNewTask() {
-        val newTask = Task(
-            taskName = binding?.taskName?.text.toString(),
-            taskDescription = binding?.taskDescription?.text.toString(),
-            taskStartDate = binding?.startDate?.text.toString(),
-            taskDueDate = binding?.dueDate?.text.toString(),
-            taskAssignedTo = binding?.personName?.text.toString()
-        )
-        DataManager.addTask(newTask)
-        adapter.setData(DataManager.tasks)
-    }
-    //endregion
 
     //region deleteTaskAt
     /**
